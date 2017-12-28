9fa054ba9723a3cb24b48a97fcd3ff7b05f9b7e4
Anchal Raheja <anchalpranav@gmail.com>
2017-12-28 16:18:29 -0600

    Update tracker.py
    price alert

--- tracker.py
+++ tracker.py
@@ -2,28 +2,23 @@ import requests
 import json
 import urllib3
 import urllib2
+import winsound
+from playsound import playsound
+import time
 
 
 print ('*'* 150)
-urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-http = urllib3.PoolManager()
-r = http.request('GET',"https://bittrex.com/api/v1.1/public/getmarketsummary?market=btc-ltc")
-json_data = json.loads(r.data.decode('UTF-8'))
-#print (json_data)
-# print ('*'* 150)
-# #print ('message',json_data['Low'])
-
-# # print ('items', json_data.items())
-# print ('+'* 150)
-
-
-# print ('keys', json_data.keys())
-# print ('+'* 150)
+for x in xrange(1,1000):
+	pass
+	urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+	http = urllib3.PoolManager()
+	r = http.request('GET',"https://bittrex.com/api/v1.1/public/getticker?market=btc-ltc")
+	json_data = json.loads(r.data.decode('UTF-8'))
+	last = json_data['result']['Last']
+	if last > 0.01711000:
+		print last
+		print time.strftime("%Y-%m-%d %H:%M:%S")
+		playsound('2.mp3')
 
 
-
-# print ('values', json_data.values())
-# print ('+'* 150)
-
-print (json_data['result'][0]['High'])
