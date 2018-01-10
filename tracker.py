9472df9475bd3885e6925d0cc393a777d61eff70
Anchal Raheja <anchalpranav@gmail.com>
2018-01-10 09:14:56 -0600

    Update tracker.py
    take input from user

--- tracker.py
+++ tracker.py
@@ -6,19 +6,28 @@ import winsound
 from playsound import playsound
 import time
 
+coin = raw_input("COIN NAME? ")
+print coin
 
+less_val= raw_input("Less Than Value ?")
+print less_val
 print ('*'* 150)
-
-for x in xrange(1,1000):
-	pass
+i = 1
+while i < 5:
+	print time.strftime("%S")
 	urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 	http = urllib3.PoolManager()
-	r = http.request('GET',"https://bittrex.com/api/v1.1/public/getticker?market=btc-ltc")
+	r = http.request('GET',"https://bittrex.com/api/v1.1/public/getticker?market=btc-"+coin)
 	json_data = json.loads(r.data.decode('UTF-8'))
 	last = json_data['result']['Last']
-	if last > 0.01711000:
+	print last
+	if last < less_val:
 		print last
 		print time.strftime("%Y-%m-%d %H:%M:%S")
 		playsound('2.mp3')
+		i = 6
+
+
+
 
 
